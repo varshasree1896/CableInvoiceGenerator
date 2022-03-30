a7ebfe26e3222641472ef586c5fe37b9f1963e6f
Pooja Sree <you@example.com>
2022-03-30 19:15:47 +0530

    Added:welcome message

---
+++ src/main/java/com/blz/cableinvoicegenerator/CableInvoiceGenerator.java
@@ -0,0 +1,8 @@
+package com.blz.cableinvoicegenerator;
+
+public class CableInvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("welcome to Cable Invoice Generator");
+
+    }
+}
---
+++ src/test/java/com/blz/cableinvoicegenerator/CableInvoiceGeneratorTest.java
@@ -0,0 +1,11 @@
+package com.blz.cableinvoicegenerator;
+
+import org.junit.jupiter.api.Test;
+
+public class CableInvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTimeWhenComputedShouldReturnTotalFare() {
+
+    }
+}
